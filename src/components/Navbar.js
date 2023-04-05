4ed02b6693f005d857ee8945a98ac81024295612
sadiq <mail@sadiq-pasha.com>
2023-04-04 21:15:38 -0700

    added drawer menu branch. Working on consolidating buttons into a single drawer.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,24 @@
 // material UI imports
-import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import Toolbar from '@mui/material/Toolbar'
+import AppBar from '@mui/material/AppBar'
 import IconButton from '@mui/material/IconButton'
 import Flight from '@mui/icons-material/Flight'
-import Typography from '@mui/material/Typography'
+import MenuIcon from '@mui/icons-material/Menu'
+import Drawer from '@mui/material/Drawer'
+import Divider from '@mui/material/Divider'
+import { useState } from 'react'
 
 // react router imports
 import { Link } from 'react-router-dom'
 
 // navbar component
 export default function ButtonAppBar() {
+
+  const [navbarMenuOpen, setNavbarMenuOpen] = useState(false)
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" style={{ background: '#3E193E' }}>
@@ -36,30 +43,63 @@ export default function ButtonAppBar() {
             Enroute to LAX
             </Typography>
           </Box>
-          <Link to="/about" style={{ textDecoration: 'none'}}>
-            <Button sx={{ 
-              mr: 2,
-              backgroundColor: '#17BEBB',
-              color: '#000000',
-              '&:hover': {
-                backgroundColor: '#D90368',
-              }    
-            }}>
+          <IconButton
+            onClick={() => setNavbarMenuOpen(true)}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ 
+              m: 1,
+              color: '#17BEBB',
+              '&:hover': { color: '#D90368'}    
+            }}
+          >
+            <MenuIcon sx={{height:30, width:30}} />
+          </IconButton>
+          <Drawer
+            anchor='right'
+            open={navbarMenuOpen}
+            onClose={() => setNavbarMenuOpen(false)}
+            PaperProps={{
+              sx: {
+                backgroundColor: '#3E193E',
+              }
+            }}
+          >
+            <Box
+              sx={{ width:200, overflowX:'hidden' }}
+              role="presentation"
+              onClick={() => setNavbarMenuOpen(false)}
+              onKeyDown={() => setNavbarMenuOpen(false)}
+              textAlign='left'            >
+              <Link to="/about" style={{ textDecoration: 'none'}}>
+                <Typography variant='text' sx={{ 
+                  width:200,
+                  mr: 2,
+                  color: '#17BEBB',
+                  '&:hover': {
+                    color: '#D90368',
+                  }    
+                }}>
                 ABOUT
-            </Button>
-          </Link>
-          <Link to="/laxinfo" style={{ textDecoration: 'none'}}>
-            <Button sx={{ 
-              mr: 2,
-              backgroundColor: '#17BEBB',
-              color: '#000000',
-              '&:hover': {
-                backgroundColor: '#D90368',
-              }    
-            }}>
+                </Typography>
+              </Link>
+              <Divider />
+              <Link to="/laxinfo" style={{ textDecoration: 'none'}}>
+                <Button variant='text' sx={{ 
+                  mr: 2,
+                  color: '#17BEBB',
+                  '&:hover': {
+                    color: '#D90368',
+                  }    
+                }}>
                 KLAX
-            </Button>
-          </Link>
+                </Button>
+              </Link>
+              <Divider />
+            </Box>
+          </Drawer>
         </Toolbar>
       </AppBar>
     </Box>
